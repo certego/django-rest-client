42be9515ad5c22d49666cd1927c81924f7041c97
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-10-22 20:37:55 +0530

    Fix #3: improve cli_maker

--- django_rest_client/cli_maker.py
+++ django_rest_client/cli_maker.py
@@ -8,53 +8,92 @@ from functools import partial
 
 from .api_client import APIClient
 from .exceptions import APIClientException
+from .types import TParams
 
-method_names = ["get", "retrieve", "list", "create", "update", "delete"]
-param_options = [
+
+__all__ = [
+    "make_groups_for_api_resources",
+]
+
+
+__method_names = ["get", "retrieve", "list", "create", "update", "delete"]
+__filter_param_options = [
     click.Option(
         ("--fields", "fields"),
         required=False,
         type=click.STRING,
+        help="CSV of fields to include",
     ),
     click.Option(
         ("--omit", "omit"),
         required=False,
         type=click.STRING,
+        help="CSV of fields to exclude",
     ),
     click.Option(
         ("--expand", "expand"),
         required=False,
         type=click.STRING,
+        help="CSV of fields to expand",
     ),
+]
+__list_param_options = [
     click.Option(
         ("--ordering", "ordering"),
         required=False,
         type=click.STRING,
+        help="CSV of fields with +,- prefix to perform ordering on",
     ),
     click.Option(
         ("--page", "page"),
         required=False,
         type=click.INT,
+        help="Page number",
     ),
     click.Option(
         ("--page_size", "page_size"),
         required=False,
         type=click.INT,
+        help="Number of entries in each page",
     ),
 ]
+__retrieve_param_options = [
+    click.Argument(
+        ("object_id",),
+        required=True,
+        type=click.STRING,
+    ),
+]
+__mutable_param_options = [
+    click.Argument(
+        ("json_data",),
+        required=True,
+        type=click.STRING,
+    )
+]
+
+
+def __parse_kwargs_into_params(pargs: dict) -> TParams:
+    params = TParams()
+    for key, value in pargs.items():
+        if value is not None and value != "":
+            if key in ["fields", "omit", "expand", "ordering"]:
+                params[key] = value.split(",")
+            else:
+                params[key] = value
+    return params
+
 
 if click:
 
-    @click.pass_context
     def cmd_callback(
-        ctx: click.Context,
         apiresource_method: Callable,
         object_id: str = None,
         json_data: str = None,
         **kwargs,
     ):
         try:
-            params = kwargs  # FIXME: needs proper parsing
+            params = __parse_kwargs_into_params(kwargs)
             if json_data:
                 data = json.loads(json_data)  # FIXME: needs proper parsing
                 if object_id:
@@ -68,9 +107,9 @@ if click:
                     resp = apiresource_method(object_id=object_id, params=params)
                 else:
                     resp = apiresource_method(params=params)
-            ctx.obj._logger.info(resp.data)
+            click.echo(resp.data)
         except APIClientException as exc:
-            ctx.obj._logger.error(str(exc))
+            click.echo(str(exc), err=True)
 
     def make_groups_for_api_resources(api_client_class: APIClient) -> List[click.Group]:
         groups = []
@@ -80,22 +119,22 @@ if click:
             grp_name = attrkey.lower()
             grp_commands = []
             # make commands
-            for method_name in method_names:
+            for method_name in __method_names:
                 method_fn = getattr(klass, method_name, None)
                 if method_fn is not None:
-                    params = param_options.copy()
-                    if "object_id" in method_fn.__annotations__:
-                        params.append(
-                            click.Argument(
-                                ("object_id",), required=True, type=click.STRING
-                            )
-                        )
-                    if "data" in method_fn.__annotations__:
-                        params.append(
-                            click.Argument(
-                                ("json_data",), required=True, type=click.STRING
-                            )
-                        )
+                    params = []
+                    if method_fn.__name__ == "retrieve":
+                        params.extend(__retrieve_param_options)
+                        params.extend(__filter_param_options)
+                    elif method_fn.__name__ == "list":
+                        params.extend(__list_param_options)
+                        params.extend(__filter_param_options)
+                    elif method_fn.__name__ == "update":
+                        params.extend(__retrieve_param_options)
+                        params.extend(__mutable_param_options)
+                    elif method_fn.__name__ == "create":
+                        params.extend(__mutable_param_options)
+
                     grp_commands.append(
                         click.Command(
                             name=method_name,
