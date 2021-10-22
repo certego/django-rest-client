a5644a88a173224c039a999a0c36c2ab4a8208e5
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-10-22 17:52:23 +0530

    improve `APIClient` and fix tests (#2)

--- example_project/client.py
+++ example_project/client.py
@@ -1,4 +1,5 @@
 from django_rest_client import APIClient
+from django_rest_client.types import THeaders
 
 from .resources import ExampleResource, ExampleSingletonResource
 
@@ -6,7 +7,13 @@ from .resources import ExampleResource, ExampleSingletonResource
 class ExampleClient(APIClient):
     # overwrite
     _server_url: str = "https://fake_url.com/"
-    _headers = {"User-Agent": "ExampleClient"}
+
+    @property
+    def _headers(self) -> THeaders:
+        return {
+            **super()._headers,
+            "User-Agent": "ExampleClient",
+        }
 
     # resources
     ExampleResource = ExampleResource
